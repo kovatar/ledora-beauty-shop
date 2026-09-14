e6706f9164ec8a7eda7e922d1c74bc27363b6803
kovatar <imsjd1383@gmail.com>
2026-09-14 22:38:28 +0330

    ci: add github pages action

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+   base: '/ledora-beauty-shop/', // <-- فقط این خط اضافه شد
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
